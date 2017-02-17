cbb68a2319de7135cce2ba2dcfc06767daf09021
Bhupeshraj Senthilkumar <bhupeshrajs@gmail.com>
2017-02-16 22:24:25 -0500

    Atomic Code Implementation - First Attempt

--- code_sample/spmv_atomic.c
+++ code_sample/spmv_atomic.c
@@ -1,21 +1,89 @@
 #include "genresult.cuh"
 #include <sys/time.h>
 
-__global__ void getMulAtomic_kernel(/*Arguments*/){
-    /* This is just an example empty kernel, you don't have to use the same kernel
- * name*/
+__global__ void getMulAtomic_kernel(int nz, int *rIndices, int *column_indices, float *values, int M, int N, float *vector, float *result){
+    
+    int thread_id = threadIdx.x + (blockIdx.x *blockDim.x);
+    int total_number_of_threads = blockDim.x * gridDim.x;
+    int iteration = nz % total_number_of_threads ? nz/total_number_of_threads + 1 : nz/total_number_of_threads;
+    for( int i = 0 ; i < iteration ; i++ ) {
+        
+        int data_id = thread_id + i* total_number_of_threads;
+        if( data_id < nz ) {
+            float data = values[data_id];
+            int row = rIndices[data_id];
+            int column = cIndices[data_id];
+            float multiplication_value = data * vector[column];
+            atomicAdd(&result[row],multiplication_value);
+        }
+    }
 }
 
 void getMulAtomic(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
     /*Allocate here...*/
 
+    /* Get the values required from the MatrixInfo Structure */
+    int number_of_non_zeros = mat->nz;
+    int *row_indices = mat->rIndex;
+    int *column_indices = mat->cIndex;
+    float *values = mat->val;
+    int M = mat->M;
+    int N = mat->N;
+    float *vector = vec->val;
+    float *result = res->val;
+    
+    /* Device copies of the required values */
+    int   *d_nz;
+    int   * d_rIndices;
+    int   * d_cIndices;
+    float *d_values;
+    int   *d_M;
+    int   *d_N;
+    float *d_vector;
+    float *d_result;
+    
+    
+    /* Allocate values for the device copies */
+    cudaMalloc((void**)&d_nz, sizeof(int) );
+    cudaMalloc((void**)&d_rIndices, sizeof(int)*number_of_non_zeros);
+    cudaMalloc((void**)&d_cIndices, sizeof(int)*number_of_non_zeros);
+    cudaMalloc((void**)&d_values, sizeof(float)*number_of_non_zeros);
+    cudaMalloc((void**)&d_M, sizeof(int));
+    cudaMalloc((void**)&d_N, sizeof(int));
+    cudaMalloc((void**)&d_vector, sizeof(float)*N);
+    cudaMalloc((void**)&d_result, sizeof(float)*M);
+    
+    /* Copying values from host to device */
+    cudaMemcpy(d_nz,&number_of_non_zeros,sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_rIndices,row_indices,sizeof(int)*number_of_non_zeros, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_cIndices,column_indices, sizeof(int)*number_of_non_zeros , cudaMemcpyHostToDevice);
+    cudaMemcpy(d_values,values, sizeof(float)*number_of_non_zeros,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_M,&M,sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_N,&N,sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_vector,vector, sizeof(float)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_result,result, sizeof(float)*M, cudaMemcpyHostToDevice);
+    
 		/* Sample timing code */
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     /*Invoke kernels...*/
 
+    getMulAtomic_kernel<<<blockSize,blockNum>>>(d_nz,d_rIndices,d_cIndices,d_M,d_N,d_vector,d_result);
+    
     cudaDeviceSynchronize();
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+    
+    cudaMemcpy(result,d_result,sizeof(float)*M, cudaMemcpyDeviceToHost);
+    res->val = result;
+    
     /*Deallocate.*/
+    cudaFree(d_nz);
+    cudaFree(d_rIndices);
+    cudaFree(d_cIndices);
+    cudaFree(d_values);
+    cudaFree(d_M);
+    cudaFree(d_N);
+    cudaFree(d_vector);
+    cudaFree(d_result);
 }
