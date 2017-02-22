73b4485c702a7446c61c6cb7db9ec7b86ba16fbb
Bhupeshraj Senthilkumar <bhupeshrajs@gmail.com>
2017-02-22 13:40:21 -0500

    Revert "Matrices"
    This reverts commit e7cf805145eb1d4b3013f07935df8ad96f4459b6.

--- code_sample/spmv_atomic.c
+++ code_sample/spmv_atomic.c
@@ -70,7 +70,7 @@ void getMulAtomic(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int bloc
     
     cudaDeviceSynchronize();
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+    printf("Atomic Kernel Time: %lu milli-seconds\n", 1000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000);
     
     cudaMemcpy(result,d_result,sizeof(float)*M, cudaMemcpyDeviceToHost);
     res->val = result;
--- code_sample/spmv_segment.c
+++ code_sample/spmv_segment.c
@@ -199,7 +199,7 @@ void getMulScan(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockS
     
     cudaDeviceSynchronize();
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    printf("Segmented Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+    printf("Segmented Kernel Time: %lu milli-seconds\n", 1000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000);
 
     /*Deallocate, please*/
     cudaMemcpy(result,d_result,sizeof(float)*M, cudaMemcpyDeviceToHost);
